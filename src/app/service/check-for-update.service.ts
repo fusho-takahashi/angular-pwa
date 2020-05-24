79009b5e70e750405e159ab277e9408fc525644e
fusho-takahashi <zenryaku@fusho-takahashi.me>
2020-05-24 17:02:12 +0900

    add check-for-update service

---
+++ src/app/service/check-for-update.service.ts
@@ -0,0 +1,19 @@
+import { Injectable, ApplicationRef } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
+import { first } from 'rxjs/operators';
+import { interval, concat } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CheckForUpdateService {
+  constructor(appRef: ApplicationRef, updates: SwUpdate) {
+    const appIsStable$ = appRef.isStable.pipe(
+      first((isStable) => isStable === true)
+    );
+    const everySixHours$ = interval(6 * 60 * 60 * 1_000);
+    const everySixHoursOnceAppIsStable$ = concat(appIsStable$, everySixHours$);
+
+    everySixHoursOnceAppIsStable$.subscribe(() => updates.checkForUpdate());
+  }
+}
